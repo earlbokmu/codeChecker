18cebb1b0a49e0d67abc121b10d4fc237534dfd8
Cho Hwiyoon <gold5784_2@hanmail.net>
2025-06-27 21:25:58 +0900

    hwi post

---
+++ src/main/java/com/project/codechecker/controller/HwiRequest.java
@@ -0,0 +1,16 @@
+package com.project.codechecker.controller;
+
+public class HwiRequest {
+
+    private String name;
+
+    public HwiRequest() {} // 기본 생성자
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
